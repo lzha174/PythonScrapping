5edc6173a6fb00d4ab03ee426854920b3acc26df
oddo <min.oddo@gmail.com>
2018-05-04 15:48:40 +1200

    ve

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,13 +12,17 @@ def index(request):
     }
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    cityNames = ['Auckland', 'Wellington']
+    return render(request, 'polls/results.html', {'question': question, "names": cityNames})
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
